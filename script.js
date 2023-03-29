bcede19ce03ad192c7a9d993476fd82495719735
Prachi24599 <prachi24599@gmail.com>
2023-03-30 00:53:17 +0530

    guessing functionality

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    //when player wins
+    document.querySelector('.message').textContent = 'Correct Number 🎉';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    // when guess is too high
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High! ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! ';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    //when guess is too low
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low! ';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! ';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
